7c152f468fcd022db6be9394f881644406e0ba45
Alexis José Güere Barrera <Aloxord@users.noreply.github.com>
2017-04-20 21:11:24 -0400

    Update conexionbd.php

--- modelo/conexionbd.php
+++ modelo/conexionbd.php
@@ -2,7 +2,7 @@
 
 $host = "localhost";
 $usuario = "root";
-$password = "58581011";
+$password = "";
 $db = "ctrl_estd";
 
 $conexion = mysqli_connect($host,$usuario,$password,$db) or die ("ok... empieza a buscar el error");
